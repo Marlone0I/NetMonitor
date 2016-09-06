0db9f48f3501d3348bc9dbd7e3a6d1c513b194dd
Marlone0I <houdeyao@qq.com>
2016-09-07 00:20:42 +0800

    Add files via upload

---
+++ NetMonitor/NetMonitor/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace NetMonitor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.网卡ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripComboBox1 = new System.Windows.Forms.ToolStripComboBox();
+            this.退出ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.label1.Location = new System.Drawing.Point(18, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "0.00B/s";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.label2.Location = new System.Drawing.Point(18, 3);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "0.00B/s";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.网卡ToolStripMenuItem,
+            this.退出ToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.ShowImageMargin = false;
+            this.contextMenuStrip1.Size = new System.Drawing.Size(80, 52);
+            // 
+            // 网卡ToolStripMenuItem
+            // 
+            this.网卡ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripComboBox1});
+            this.网卡ToolStripMenuItem.Name = "网卡ToolStripMenuItem";
+            this.网卡ToolStripMenuItem.ShowShortcutKeys = false;
+            this.网卡ToolStripMenuItem.Size = new System.Drawing.Size(79, 24);
+            this.网卡ToolStripMenuItem.Text = "网卡";
+            // 
+            // toolStripComboBox1
+            // 
+            this.toolStripComboBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.toolStripComboBox1.DropDownHeight = 200;
+            this.toolStripComboBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.toolStripComboBox1.IntegralHeight = false;
+            this.toolStripComboBox1.MaxDropDownItems = 10;
+            this.toolStripComboBox1.Name = "toolStripComboBox1";
+            this.toolStripComboBox1.Size = new System.Drawing.Size(300, 27);
+            // 
+            // 退出ToolStripMenuItem
+            // 
+            this.退出ToolStripMenuItem.Name = "退出ToolStripMenuItem";
+            this.退出ToolStripMenuItem.Size = new System.Drawing.Size(79, 24);
+            this.退出ToolStripMenuItem.Text = "退出";
+            this.退出ToolStripMenuItem.Click += new System.EventHandler(this.退出ToolStripMenuItem_Click);
+            // 
+            // ToolStripMenuItem
+            // 
+            this.ToolStripMenuItem.Name = "ToolStripMenuItem";
+            this.ToolStripMenuItem.Size = new System.Drawing.Size(32, 19);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(76, 5);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(11, 12);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "0";
+            this.label3.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(76, 23);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(11, 12);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "0";
+            this.label4.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(90, 40);
+            this.ContextMenuStrip = this.contextMenuStrip1;
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Opacity = 0.9D;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.TopMost = true;
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 网卡ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 退出ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripComboBox toolStripComboBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ NetMonitor/NetMonitor/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Timers;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Net.NetworkInformation;
+
+
+namespace NetMonitor
+{
+    public partial class Form1 : Form
+    {        
+        private NetworkInterface[] nicArr;      //网卡集合
+        private System.Timers.Timer timer;      //计时器
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            InitNetworkInterface();
+            InitializeTimer();
+        }
+
+        /// <summary>
+        /// 初始化网卡
+        /// </summary>
+        public void InitNetworkInterface()
+        {
+            nicArr = NetworkInterface.GetAllNetworkInterfaces();
+            for (int i = 0; i < nicArr.Length; i++)
+                toolStripComboBox1.Items.Add(nicArr[i].Name);
+            toolStripComboBox1.SelectedIndex = 0;
+        }
+
+        /// <summary>
+        /// 初始化计时器
+        /// </summary>
+        private void InitializeTimer()
+        {
+            timer = new System.Timers.Timer();
+            timer.Interval = 1000;
+            timer.Elapsed += timer_Elapsed;
+            timer.Start();
+        }
+               
+        void timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            this.Invoke((EventHandler)delegate
+            {
+                UpdateNetworkInterface();
+            });
+        }
+
+        public void UpdateNetworkInterface()
+        {
+            NetworkInterface nic = nicArr[toolStripComboBox1.SelectedIndex];
+            IPv4InterfaceStatistics interfaceStats = nic.GetIPv4Statistics();
+
+            int bytesSent = (int)(interfaceStats.BytesSent - double.Parse(label4.Text));
+            int bytesRecv = (int)(interfaceStats.BytesReceived - double.Parse(label3.Text));
+            
+            label3.Text = interfaceStats.BytesReceived.ToString();
+            label4.Text = interfaceStats.BytesSent.ToString();
+
+            
+
+            string netRecvText = "";
+            string netSendText = "";
+
+            
+            if (bytesRecv < 1024 * 1024)
+            {
+                netRecvText = ((double)bytesRecv / 1024).ToString("0.00") + "K/s";
+            }
+            else if (bytesRecv >= 1024 * 1024)
+            {
+                netRecvText = ((double)bytesRecv / (1024 * 1024)).ToString("0.00") + "M/s";
+            }
+
+            
+            if (bytesSent < 1024 * 1024)
+            {
+                netSendText = ((double)bytesSent / 1024).ToString("0.00") + "K/s";
+            }
+            else if (bytesSent >= 1024 * 1024)
+            {
+                netSendText = ((double)bytesSent / (1024 * 1024)).ToString("0.00") + "M/s";
+            }
+
+            //更新控件
+
+            label1.Text = netSendText;
+            label2.Text = netRecvText;
+        }
+
+
+        /// <summary>
+        /// 嵌入状态栏
+        /// </summary>
+        [DllImport("user32.dll")]
+        static extern IntPtr FindWindow(String lpClassName, String lpWindowName);
+        [DllImport("user32.dll")]
+        static extern IntPtr FindWindowEx(IntPtr hWnd1, IntPtr hWnd2, String lpsz1, String lpsz2);
+        [DllImport("user32.dll")]
+        static extern IntPtr SetParent(IntPtr hWnd, IntPtr hWndNewParent);
+
+        [DllImport("user32.dll")]
+        static extern IntPtr MoveWindow(IntPtr hWnd, IntPtr x, IntPtr y, int nWidth, int nHeight, int bRepaint);
+
+        [DllImport("user32.dll")]
+        static extern IntPtr GetWindowRect(IntPtr HWnd, out Rect lpRect);
+
+
+        public struct Rect
+        {
+            public int Left;
+            public int Top;
+            public int Right;
+            public int Bottom;
+        }
+        private IntPtr hMin;
+        private Rect rcMin;
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            IntPtr hTaskbar = FindWindow("Shell_TrayWnd", null);
+            IntPtr hBar = FindWindowEx(hTaskbar, (IntPtr)0, "ReBarWindow32", null);
+            hMin = FindWindowEx(hBar, (IntPtr)0, "MSTaskSwWClass", null);
+
+            Rect rcBar = new Rect();
+            rcMin = new Rect();
+            GetWindowRect(hMin, out rcMin);
+            GetWindowRect(hBar, out rcBar);
+
+            MoveWindow(hMin, (IntPtr)0, (IntPtr)0, rcMin.Right - rcMin.Left - this.Width, rcMin.Bottom - rcMin.Top, 1);
+
+            SetParent(this.Handle, hBar);
+            this.Height = 40;
+            this.Width = this.Width;
+            MoveWindow(this.Handle, (IntPtr)(rcMin.Right - rcMin.Left - this.Width + 2), (IntPtr)((rcBar.Bottom - rcBar.Top - this.Height) / 2), this.Width, this.Height, 1);
+        }
+
+        private void 退出ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }    
+    }
+}
